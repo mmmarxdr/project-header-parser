20d6ec6db96328187861cd2d4ac732d77ce8b60e
marc <marxdr7@gmail.com>
2023-09-08 16:53:54 -0300

    Projecto Finalizado

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function(req, res) {
+  const ipAdress = req.ip;
+  const preferredLanguage = req.header['accept-language'];
+  const software = req.header['user-agent'];
+
+  res.json({
+    ipadress: ipAdress,
+    language: preferredLanguage,
+    software: software
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
